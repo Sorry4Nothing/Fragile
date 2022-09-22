2f16f9ed381bb5ed432cbf268f9a895947c013e1
dlanaras <dimitrislanaras04@outlook.com>
2022-09-22 16:05:33 +0200

    feat: simple hardcoded login

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from 'express';
+const app = express();
+const port = 3000;
+import bodyParser from 'body-parser';
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello Planet!');
+	res.send('Hello Planet!');
 });
 
-app.post('login', (req, res) => {
-    req.
+app.post('/login', (req, res) => {
+	let username = req.body.username;
+	let password = req.body.password;
+
+	if (username == 'password' && password == 'password') {
+		res.send(`Welcome, your password is ${password}`);
+    } else {
+        res.send('GET LOST!');
+    }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
