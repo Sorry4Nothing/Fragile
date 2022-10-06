788d79124de0f764668d842d7fba4ad49b90ed4b
sarabilling94 <sara.billing@gmx.ch>
2022-10-06 16:33:19 +0200

    feat: details page WIP

---
+++ frontend/src/details_window.ts
@@ -0,0 +1,75 @@
+import Adw from '@gi/adw1';
+import { registerClass } from '@/utils/gjs';
+import GObject from '@gi/gobject2';
+import Gio from '@gi/gio2';
+import GLib from '@gi/glib2';
+import Gtk from '@gi/gtk4';
+
+Gio._promisify(Gio.File.prototype, 'replace_contents_async', 'replace_contents_finish');
+
+@registerClass({
+	GTypeName: 'FragDetailsWindow',
+	Template: 'resource:///com/github/sorry4nothing/Fragile/details_window.ui',
+	Properties: {
+        'tasks_mock': GObject.param_spec_variant('testlist', 'Testlist', 'Testlist',
+        new GLib.VariantType('as'), null,
+        GObject.ParamFlags.READWRITE),
+	},
+    Children: [
+        'dropdown_projects',
+        'list_backlog',
+        'list_open',
+        'list_reviewready',
+        'list_done',
+    ],
+})
+export class FragDetailsWindow extends Adw.ApplicationWindow {
+    public dropdown_projects: Gtk.StringList;
+    public list_backlog: Gtk.ListBox;
+    public list_open: Gtk.ListBox;
+    public list_reviewready: Gtk.ListBox;
+    public list_done: Gtk.ListBox;
+
+    //later to be replaced with real data
+    public tasks_mock = ["Task1", "Task2", "Task3", "Task4"];
+
+    constructor(params = {}) {
+        super(params);
+
+        this.dropdown_projects.append("test1");
+
+        this.tasks_mock.forEach(task => {
+            const gtkText = new Gtk.Label();
+            gtkText.label = task;
+            const gtkText2 = new Gtk.Label();
+            gtkText2.label = task;
+            const gtkText3 = new Gtk.Label();
+            gtkText3.label = task;
+            const gtkText4 = new Gtk.Label();
+            gtkText4.label = task;
+
+            this.list_backlog.append(gtkText);
+            this.list_backlog.show();
+
+            this.list_open.append(gtkText2);
+            this.list_open.show();
+
+            this.list_reviewready.append(gtkText3);
+            this.list_reviewready.show();
+
+            this.list_done.append(gtkText4);
+            this.list_done.show();
+
+        });
+
+        this.list_backlog.visible = true;
+        this.list_open.visible = true;
+        this.list_reviewready.visible = true;
+        this.list_done.visible = true;
+    }
+
+    // fetch data
+	on_refresh_clicked() {
+        console.log("to be implemented");
+	}
+}
