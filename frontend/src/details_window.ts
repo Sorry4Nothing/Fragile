9ae3a829e45f113e89e688e15768ae3ead66cba3
sarabilling94 <sara.billing@gmx.ch>
2022-11-09 15:38:15 +0100

    use FragileProject model for mock data

--- frontend/src/details_window.ts
+++ frontend/src/details_window.ts
@@ -5,6 +5,7 @@ import Gio from '@gi/gio2';
 import GLib from '@gi/glib2';
 import Gtk from '@gi/gtk4';
 import { FragImportWindow } from './import_window';
+import { FragileProject } from './models/fragileproject';
 
 Gio._promisify(Gio.File.prototype, 'replace_contents_async', 'replace_contents_finish');
 
@@ -12,14 +13,6 @@ Gio._promisify(Gio.File.prototype, 'replace_contents_async', 'replace_contents_f
 	GTypeName: 'FragDetailsWindow',
 	Template: 'resource:///com/github/sorry4nothing/Fragile/details_window.ui',
 	Properties: {
-		tasks_mock: GObject.param_spec_variant(
-			'testlist',
-			'Testlist',
-			'Testlist',
-			new GLib.VariantType('as'),
-			null,
-			GObject.ParamFlags.READWRITE
-		),
 		projects_mock: GObject.param_spec_variant(
 			'testlist',
 			'Testlist',
@@ -32,6 +25,7 @@ Gio._promisify(Gio.File.prototype, 'replace_contents_async', 'replace_contents_f
 	Children: ['sidebar', 'list_backlog', 'list_open', 'list_reviewready', 'list_done'],
 })
 export class FragDetailsWindow extends Adw.ApplicationWindow {
+	public project: FragileProject;
 	public list_backlog: Gtk.ListBox;
 	public list_open: Gtk.ListBox;
 	public list_reviewready: Gtk.ListBox;
@@ -39,11 +33,11 @@ export class FragDetailsWindow extends Adw.ApplicationWindow {
 	public sidebar: Gtk.ListBox;
 
 	// TODO: replace with real data
-	public tasks_mock = ['Task1', 'Task2', 'Task3', 'Task4'];
 	public projects_mock = ["Project1", "Project2", "Project3"];
 
-	constructor(params = { }) {
-		super(params);
+	constructor(application: Gtk.Application, project: FragileProject) {
+		super({application: application})
+		this.project = project;
 
 		this.projects_mock.forEach(project => {
 			const gtkButton = new Gtk.Button();
@@ -55,27 +49,28 @@ export class FragDetailsWindow extends Adw.ApplicationWindow {
 			this.sidebar.show();
 		})
 
-		this.tasks_mock.forEach((task) => {
-			const gtkText = new Gtk.Label();
-			gtkText.label = task;
-			const gtkText2 = new Gtk.Label();
-			gtkText2.label = task;
-			const gtkText3 = new Gtk.Label();
-			gtkText3.label = task;
-			const gtkText4 = new Gtk.Label();
-			gtkText4.label = task;
-
+		const backlog = this.project.columns.find(c => c.name == "backlog");
+		backlog?.missions.forEach(m => {
+			const gtkText = new Gtk.Label({label: `${m.missionType} | ${m.missionName}: ${m.missionDescription}`});
 			this.list_backlog.append(gtkText);
-			this.list_backlog.show();
+		});
 
-			this.list_open.append(gtkText2);
-			this.list_open.show();
+		const open = this.project.columns.find(c => c.name == "open");
+		open?.missions.forEach(m => {
+			const gtkText = new Gtk.Label({label: `${m.missionType} | ${m.missionName}: ${m.missionDescription}`});
+			this.list_open.append(gtkText);
+		});
 
-			this.list_reviewready.append(gtkText3);
-			this.list_reviewready.show();
+		const review = this.project.columns.find(c => c.name == "review");
+		review?.missions.forEach(m => {
+			const gtkText = new Gtk.Label({label: `${m.missionType} | ${m.missionName}: ${m.missionDescription}`});
+			this.list_reviewready.append(gtkText);
+		});
 
-			this.list_done.append(gtkText4);
-			this.list_done.show();
+		const done = this.project.columns.find(c => c.name == "done");
+		done?.missions.forEach(m => {
+			const gtkText = new Gtk.Label({label: `${m.missionType} | ${m.missionName}: ${m.missionDescription}`});
+			this.list_done.append(gtkText);
 		});
 
 		this.list_backlog.visible = true;
@@ -86,7 +81,7 @@ export class FragDetailsWindow extends Adw.ApplicationWindow {
 
 	// TODO: open for real project, when we have the data
 	on_project_clicked(){
-		const win = new FragDetailsWindow({ application: this.application });
+		const win = new FragDetailsWindow(this.application, this.project);
 		this.close();
 		win.show();
 	}
